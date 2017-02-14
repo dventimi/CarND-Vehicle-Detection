4d57fff1dfc87109b35faa10282fdfd853071b0a
David A. Ventimiglia <dventimi@gmail.com>
2017-02-14 15:50:25 -0800

    Combining Features

--- detect.py
+++ detect.py
@@ -341,10 +341,37 @@ plt.title('HOG Visualization')
 # 21. Combine and Normalize Features
 # https://classroom.udacity.com/nanodegrees/nd013/parts/fbf77062-5703-404e-b60c-95b78b2f3f9e/modules/2b62a1c3-e151-4a0e-b6b6-e424fa46ceab/lessons/fd66c083-4ccb-4fe3-bda1-c29db76f50a0/concepts/cacf86d7-f8eb-46bd-9f09-34a2ff208ce8
 
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.preprocessing import StandardScaler
+import glob
+
+# Define a function to compute binned color features  
+def bin_spatial(img, size=(32, 32)):
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(img, size).ravel() 
+    # Return the feature vector
+    return features
+
+
+# Define a function to compute color histogram features  
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+
+
 # Define a function to extract features from a list of images
 # Have this function call bin_spatial() and color_hist()
 def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
-                        hist_bins=32, hist_range=(0, 256)):
+                     hist_bins=32, hist_range=(0, 256)):
     # Create a list to append feature vectors to
     features = []
     # Iterate through the list of images
@@ -372,6 +399,35 @@ def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
     return features
 
 
+car_features = extract_features(cars, cspace='RGB', spatial_size=(32, 32),
+                                hist_bins=32, hist_range=(0, 256))
+notcar_features = extract_features(notcars, cspace='RGB', spatial_size=(32, 32),
+                                   hist_bins=32, hist_range=(0, 256))
+
+if len(car_features) > 0:
+    # Create an array stack of feature vectors
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)                        
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+    car_ind = np.random.randint(0, len(cars))
+    # Plot an example of raw and scaled features
+    fig = plt.figure(figsize=(12,4))
+    plt.subplot(131)
+    plt.imshow(mpimg.imread(cars[car_ind]))
+    plt.title('Original Image')
+    plt.subplot(132)
+    plt.plot(X[car_ind])
+    plt.title('Raw Features')
+    plt.subplot(133)
+    plt.plot(scaled_X[car_ind])
+    plt.title('Normalized Features')
+    fig.tight_layout()
+else: 
+    print('Your function only returns empty feature vectors...')
+
+
 # 26. Color Classify
 # https://classroom.udacity.com/nanodegrees/nd013/parts/fbf77062-5703-404e-b60c-95b78b2f3f9e/modules/2b62a1c3-e151-4a0e-b6b6-e424fa46ceab/lessons/fd66c083-4ccb-4fe3-bda1-c29db76f50a0/concepts/be308636-742b-416a-8fcc-c6071865a11f
 
@@ -389,12 +445,12 @@ from sklearn.preprocessing import StandardScaler
 # from sklearn.model_selection import train_test_split
 from sklearn.cross_validation import train_test_split
 
-# Define a function to compute binned color features  
-def bin_spatial(img, size=(32, 32)):
-    # Use cv2.resize().ravel() to create the feature vector
-    features = cv2.resize(img, size).ravel() 
-    # Return the feature vector
-    return features
+# # Define a function to compute binned color features  
+# def bin_spatial(img, size=(32, 32)):
+#     # Use cv2.resize().ravel() to create the feature vector
+#     features = cv2.resize(img, size).ravel() 
+#     # Return the feature vector
+#     return features
 
 # Define a function to compute color histogram features  
 def color_hist(img, nbins=32, bins_range=(0, 256)):
