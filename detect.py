7c9df4462a938589b777def7c3c0b1fd0d702228
David A. Ventimiglia <dventimi@gmail.com>
2017-02-14 09:34:03 -0800

    Histograms of Color quiz complete

--- detect.py
+++ detect.py
@@ -77,9 +77,15 @@ bboxes = find_matches(image, templist)
 result = draw_boxes(image, bboxes)
 plt.imshow(result)
 
-
 # 12. Histograms of Color
 # https://classroom.udacity.com/nanodegrees/nd013/parts/fbf77062-5703-404e-b60c-95b78b2f3f9e/modules/2b62a1c3-e151-4a0e-b6b6-e424fa46ceab/lessons/fd66c083-4ccb-4fe3-bda1-c29db76f50a0/concepts/4f0692c6-e22d-4f28-b5d0-7990a4d8de86
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+image = mpimg.imread('cutout1.jpg')
 
 # Define a function to compute color histogram features  
 def color_hist(img, nbins=32, bins_range=(0, 256)):
@@ -95,6 +101,28 @@ def color_hist(img, nbins=32, bins_range=(0, 256)):
     # Return the individual histograms, bin_centers and feature vector
     return rhist, ghist, bhist, bin_centers, hist_features
     
+
+rh, gh, bh, bincen, feature_vec = color_hist(image, nbins=32, bins_range=(0, 256))
+
+# Plot a figure with all three bar charts
+if rh is not None:
+    fig = plt.figure(figsize=(12,3))
+    plt.subplot(131)
+    plt.bar(bincen, rh[0])
+    plt.xlim(0, 256)
+    plt.title('R Histogram')
+    plt.subplot(132)
+    plt.bar(bincen, gh[0])
+    plt.xlim(0, 256)
+    plt.title('G Histogram')
+    plt.subplot(133)
+    plt.bar(bincen, bh[0])
+    plt.xlim(0, 256)
+    plt.title('B Histogram')
+    fig.tight_layout()
+else:
+    print('Your function is returning None for at least one variable...')
+
 
 # 15. Spatial Binning of Color
 # https://classroom.udacity.com/nanodegrees/nd013/parts/fbf77062-5703-404e-b60c-95b78b2f3f9e/modules/2b62a1c3-e151-4a0e-b6b6-e424fa46ceab/lessons/fd66c083-4ccb-4fe3-bda1-c29db76f50a0/concepts/404dfd70-937b-468f-a3df-5fb88cc2f765
