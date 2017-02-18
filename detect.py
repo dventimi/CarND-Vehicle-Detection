558e6b410a4ef1b40dd4f9304b4f6d1b7ec03d18
David A. Ventimiglia <dventimi@gmail.com>
2017-02-17 16:33:06 -0800

    Working on the classifier

--- detect.py
+++ detect.py
@@ -1,5 +1,7 @@
+from glob import glob
 from itertools import groupby, islice, zip_longest, cycle, filterfalse, chain
 from moviepy.editor import VideoFileClip, VideoClip
+from random import choice, sample
 from skimage import color, exposure
 from skimage.feature import hog
 from sklearn.cross_validation import train_test_split
@@ -7,43 +9,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 import cv2
-import glob
 import math
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-class HyperParameters:
-    def __init__(self):
-        pass
-
-
-global Theta
-Theta = HyperParameters()
-Theta.colorspace = 'RGB'
-Theta.orient = 9
-Theta.pix_per_cell = 8
-Theta.cell_per_block = 2
-Theta.hog_channel = 0
-
-def extract_features(img):
-    X = np.array([])
-    X = np.append(X, hog(cv2.cvtColor(img, cv2.COLOR_RGB2HLS)[:,:,0]))
-    s = StandardScaler().fit(X)
-    return s.transform(X)
-
-
-X,y = zip(*chain(((extract_features(mpimg.imread(f)), 1) for f in glob.glob('vehicles_smallset/**/*.jpeg')),
-                 ((extract_features(mpimg.imread(f)), 0) for f in glob.glob('non-vehicles_smallset/**/*.jpeg'))))
-
-def get_classifier(X, y):
-    svc = LinearSVC()
-    svc.fit(X, y)
-    return svc
-
-
-c = get_classifier(X,y)
 
 
 crt2cyl = lambda x,y,z: (math.sqrt(x**2+y**2), math.atan2(y,x), z)
@@ -54,7 +23,17 @@ crt2sph = lambda x,y,z: (math.sqrt(x**2+y**2+z**2), math.acos(z/math.sqrt(x**2+y
 sph2crt = lambda r,theta,phi: (r*math.sin(theta)*math.cos(phi), r*math.sin(theta)*math.sin(phi), r*math.cos(theta))
 
 
-def draw_patch(img, x, y, z, horizon=0.5, width=4, height=4, color=(0,0,255), thick=6):
+feed = lambda pattern, y: ((f, y) for f in glob(pattern))
+shuffle = lambda l: sample(l, len(l))
+load = lambda g: ((mpimg.imread(x[0]),x[1]) for x in g)
+flip = lambda g: ((x[0][:,::-1,:],x[1]) for x in g)
+mirror = lambda g: chain(g, flip(g))
+group = lambda items, n, fillvalue=None: zip_longest(*([iter(items)]*n), fillvalue=fillvalue)
+transpose = lambda tuples: (list(map(list, zip(*g))) for g in tuples)
+batch = lambda groups, indices=[0, 1]: ([np.asarray(t[i]) for i in indices] for t in groups)
+
+
+def get_patch(img, x, y, z, horizon=0.5, width=4, height=4):
     d = 1
     r,theta,phi = crt2sph(x,y,z)
     rho2 = d*math.tan(theta)
@@ -65,17 +44,12 @@ def draw_patch(img, x, y, z, horizon=0.5, width=4, height=4, color=(0,0,255), th
     dx = int(width/2*scale/z)
     dy = int(height/2*scale/z)
     bbox = [(center[0]-dx,center[1]-dy), (center[0]+dx,center[1]+dy)]
+    return (bbox,center)
+
+
+def draw_patch(img, bbox, color=(0,0,255), thick=3):
     cv2.rectangle(img, bbox[0], bbox[1], color, thick)
     return img
-
-
-image = mpimg.imread('bbox-example-image.jpg')
-for d in 2**np.arange(6)*5:
-    plt.imshow(draw_patch(image, -15, -1.5, d, horizon=0.3, color=(0,255,255))) 
-    plt.imshow(draw_patch(image, -7,  -1.5, d, horizon=0.3, color=(255,0,0)))
-    plt.imshow(draw_patch(image,  0,  -1.5, d, horizon=0.3, color=(255,255,255)))
-    plt.imshow(draw_patch(image,  7,  -1.5, d, horizon=0.3, color=(0,255,0)))
-    plt.imshow(draw_patch(image,  15, -1.5, d, horizon=0.3, color=(255,0,255)))
 
 
 def get_frame_maker(img):
@@ -84,11 +58,11 @@ def get_frame_maker(img):
     def make_frame(t):
         frame = np.copy(img)
         z = 2**(t % 5)*5
-        draw_patch(frame, -15, y, z, horizon=0.30, color=(0,255,255))
-        draw_patch(frame, -7, y, z, horizon=0.30, color=(0,255,255))
-        draw_patch(frame, 0, y, z, horizon=0.30, color=(0,255,255))
-        draw_patch(frame, 7, y, z, horizon=0.30, color=(0,255,255))
-        draw_patch(frame, 15, y, z, horizon=0.30, color=(0,255,255))
+        draw_patch(frame, get_patch(frame,-15,y,z,horizon=0.30)[1], color=(0,255,255))
+        draw_patch(frame, get_patch(frame,-7,y,z,horizon=0.30)[1], color=(0,255,255))
+        draw_patch(frame, get_patch(frame,0,y,z,horizon=0.30)[1], color=(0,255,255))
+        draw_patch(frame, get_patch(frame,7,y,z,horizon=0.30)[1], color=(0,255,255))
+        draw_patch(frame, get_patch(frame,15,y,z,horizon=0.30)[1], color=(0,255,255))
         return frame
     return make_frame
 
@@ -96,4 +70,90 @@ def get_frame_maker(img):
 clip = VideoClip(get_frame_maker(mpimg.imread('bbox-example-image.jpg')), duration=5)
 clip.write_videofile("output_images/patches1.mp4", fps=25)
 
-a = cycle(np.mgrid[-10:10:5,2:100:30].T.reshape(-1,2))
+
+class HyperParameters:
+    def __init__(self):
+        pass
+
+
+global Theta
+Theta = HyperParameters()
+Theta.colorspace = cv2.COLOR_RGB2HSV
+Theta.channel = 1
+Theta.orient = 9
+Theta.pix_per_cell = 8
+Theta.cell_per_block = 2
+Theta.transform_sqrt = False
+Theta.feature_vec = True
+
+def extract_features(img):
+    X = np.array([])
+    X = np.append(X, hog(cv2.cvtColor(img, Theta.colorspace)[:,:,0],
+                         Theta.orient,
+                         (Theta.pix_per_cell,Theta.pix_per_cell),
+                         (Theta.cell_per_block,Theta.cell_per_block),
+                         transform_sqrt = Theta.transform_sqrt,
+                         feature_vector = Theta.feature_vec))
+    X = np.append(X, hog(cv2.cvtColor(img, Theta.colorspace)[:,:,1],
+                         Theta.orient,
+                         (Theta.pix_per_cell,Theta.pix_per_cell),
+                         (Theta.cell_per_block,Theta.cell_per_block),
+                         transform_sqrt = Theta.transform_sqrt,
+                         feature_vector = Theta.feature_vec))
+    X = np.append(X, hog(cv2.cvtColor(img, Theta.colorspace)[:,:,2],
+                         Theta.orient,
+                         (Theta.pix_per_cell,Theta.pix_per_cell),
+                         (Theta.cell_per_block,Theta.cell_per_block),
+                         transform_sqrt = Theta.transform_sqrt,
+                         feature_vector = Theta.feature_vec))
+    s = StandardScaler().fit(X)
+    return s.transform(X)
+
+
+def get_classifier(X, y):
+    svc = LinearSVC()
+    svc.fit(X, y)
+    return svc
+
+samples = list(chain(feed("vehicles/**/*.png",1),feed("non-vehicles/**/*.png",0)))
+data = cycle(mirror(load(shuffle(samples))))
+
+Theta.colorspace = cv2.COLOR_RGB2HSV
+Theta.channel = 2
+Theta.orient = 15
+Theta.pix_per_cell = 16
+Theta.cell_per_block = 4
+Theta.transform_sqrt = False
+Theta.feature_vec = True
+
+X_train,X_test,y_train,y_test = train_test_split(*zip(*((extract_features(s[0]), s[1]) for s in islice(data, len(samples)))), test_size=0.2, random_state=np.random.randint(0, 100))
+classifier = get_classifier(X_train,y_train)
+print('Test Accuracy of SVC = ', round(classifier.score(X_test, y_test), 4))
+
+
+def get_processor(image):
+    grid = [get_patch(image, *x) for x in np.mgrid[-2:-0.5:0.1,
+                                                   -16:16:1,
+                                                   10:100:10].T.reshape(-1,3)]
+    def process_image(img):
+        frame = np.copy(img)
+        heat = np.zeros_like(image[:,:,0]).astype(np.float)
+        for window,center in grid:
+            import pdb
+            pdb.set_trace()
+            test_img = cv2.resize(frame[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))
+            features = extract_features(test_img)
+            prediction = clf.predict(test_features)
+            if prediction==1:
+                np.zeros[center]+=1
+        return heat
+    return process_image
+
+
+in_clip = VideoFileClip("project_video.mp4")
+out_clip = in_clip.fl_image(get_processor(5))
+cProfile.run('out_clip.write_videofile("output_images/project_output.mp4", audio=False)', 'restats')
+
+
+
+        
